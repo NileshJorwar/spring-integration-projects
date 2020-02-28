0b29614ddd4c1a943bc95e75b9cc979d53ed3ed9
nileshjorwar <jorwarnilesh@gmail.com>
2020-02-27 19:16:19 -0600

    Adding spring-integration-channels repo

---
+++ spring-integration-channels/src/main/java/com/nilesh/springintegrationchannels/SpringIntegrationChannelsApplication.java
@@ -0,0 +1,13 @@
+package com.nilesh.springintegrationchannels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationChannelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegrationChannelsApplication.class, args);
+    }
+
+}
